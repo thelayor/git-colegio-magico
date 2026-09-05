6a9eb2d80bbd88e0366211f287d26814c720c86a
thelayor <juanpablocardona082@gmail.com>
2026-09-05 16:22:35 +0000

    feat: Reto 3 solucionado

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,7 +6,11 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String pociones = "";
+        for (int i = 0; i < cantidad; i++) { 
+            pociones= pociones+ "Pocion ";
+        }
+        return pociones ; 
     }
 
     public static void main(String[] args) {
