a121d57394e95a80326c62182678290b6f3fc2ae
thelayor <juanpablocardona082@gmail.com>
2026-09-08 03:01:19 +0000

    feat: Reto 6 completado

--- src/main/java/com/colegiomagico/reto6/SombreroClasificador.java
+++ src/main/java/com/colegiomagico/reto6/SombreroClasificador.java
@@ -6,7 +6,14 @@ package com.colegiomagico.reto6;
 public class SombreroClasificador {
 
     public static String clasificar(String casa) {
-        return ""; // TODO: Cambia esto
+        switch (casa) {
+            case "Gryffindor":
+                return "Valentia";
+            case "Ravenclaw":
+                return "Inteligencia";
+            default:
+                return "Desconocido";
+        }
     }
 
     public static void main(String[] args) {
