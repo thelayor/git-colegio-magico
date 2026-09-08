9f8cfc6593f48c44eb40b4ed3b3abc7dcfb93216
thelayor <juanpablocardona082@gmail.com>
2026-09-08 03:03:56 +0000

    feat: Reto 7 completado

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,9 +6,14 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int mayor = tamanos[0];
+        for (int i = 1; i < tamanos.length; i++) {
+            if (tamanos[i] > mayor) {    
+                mayor = tamanos[i];
+            }
+        }   
+        return mayor; 
     }
-
     public static void main(String[] args) {
         int[] varitas = {10, 15, 8};
         System.out.println("Poderosa: " + varitaMasPoderosa(varitas));
